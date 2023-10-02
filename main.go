8b4bbae12750b06fd328963879cab00890b0b13b
Emir Alkal <alkalemir@gmail.com>
2023-10-02 18:52:15 +0300

    added go files

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, wodrld")
+}
+
+/*
+	int8
+	uint8
+	int16
+	unit16
+	int32
+	uint32
+	int64
+	uint64
+	float32
+	float64
+	complex64
+	complex128
+	byte
+	rune
+	int
+	uint
+	bool
+	uintptr
+*/
